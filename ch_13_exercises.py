bda3ddd50b4ac164c83cf1d74894b1fe996d572c
Joe Davidson <79029363+jdavidson85@users.noreply.github.com>
2023-04-24 10:05:37 -0500

    Add files via upload

---
+++ ch_13_exercises.py
@@ -0,0 +1,37 @@
+"""
+    Starting Out with Python by Tony Gaddis, fifth edition
+    Complete all of the TODO directions
+    The number next to the TODO represents the chapter
+    and section in your textbook that explain the required code
+    Your file should compile error free (green checkmark upper right)
+    Submit your completed file
+"""
+import tkinter
+
+# TODO 13.2 Using the tkinter Module
+print("=" * 10, "Section 13.2 using tkinter", "=" * 10)
+# Write the code from program 13-2 to display an empty window, no need
+# to re-import tkinter. Use the class name MyGUI2
+
+# TODO 13.3 Adding a label widget
+print("=" * 10, "Section 13.3 adding a label widget", "=" * 10)
+# Add a label to MyGUI2 (above) that prints your first and last name; pack the label
+# Create an instance of MyGUI2
+
+# TODO 13.4 Organizing Widgets with Frames
+print("=" * 10, "Section 13.4 using frames", "=" * 10)
+# Create a MyGUI3 class that creates a window with two frames
+# In the top Frame add labels with your name and major
+# In the bottom frame add labels with the classes you are taking this semester
+# Create an instance of MyGUI3
+
+# TODO 13.5 Button Widgets and info Dialog Boxes
+print("=" * 10, "Section 13.5 button widgets and info dialogs", "=" * 10)
+# Create a GUI that will tell a joke
+# Use a button to show the punch line, which should appear in a dialog box
+# Create an instance of the GUI
+
+# TODO 13.6 getting input / 13.7 Using Labels as output fields
+print("=" * 10, "Section 13.6-13.7 input and output using Entry and Label", "=" * 10)
+# Using the program in 13.10 kilo converter as a sample,
+# create a program to convert inches to centimeters
